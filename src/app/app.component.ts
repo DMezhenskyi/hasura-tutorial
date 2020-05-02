dcaf7819fb18057824bdb2293b842e0ea47826ca
mezhik91 <mezhik91@gmail.com>
2020-05-02 18:33:05 +0200

    lesson 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Apollo, QueryRef } from 'apollo-angular';
+import gql from 'graphql-tag';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
+interface Task {
+  uuid: string;
+  title: string;
+  description: string;
+  user: User;
+}
+
+interface User {
+  uuid: string;
+  displayName: string;
+}
+interface Response {
+  tasks: Task[];
+}
+const GET_TASKS = gql`
+  query Tasks {
+    tasks {
+      uuid
+      title
+      description
+      user {
+        fullName
+        uuid
+      }
+    }
+  }
+`;
+const ADD_TASK = gql`
+  mutation AddTask($authorId: uuid!, $description: String!, $title: name!) {
+    insert_tasks(
+      objects: { authorId: $authorId, description: $description, title: $title }
+    ) {
+      returning {
+        description
+        title
+        user {
+          fullName
+          uuid
+        }
+        uuid
+      }
+    }
+  }
+`;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hasura-tutorial';
+
+  tasks$: Observable<Task[]>;
+  form: FormGroup;
+  queryRef: QueryRef<Response>;
+
+  constructor(private apollo: Apollo, private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      title: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      authorId: new FormControl(
+        '0df9d120-9c4a-4dd6-979d-1fcbd32dc8a5',
+        Validators.required
+      ),
+    });
+    this.queryRef = this.apollo.watchQuery<Response>({
+      query: GET_TASKS,
+    });
+    this.tasks$ = this.queryRef.valueChanges.pipe(
+      map((result) => result.data.tasks)
+    );
+  }
+
+  onAddTask() {
+    this.apollo
+      .mutate({
+        mutation: ADD_TASK,
+        variables: this.form.value,
+      })
+      .subscribe(
+        () => {
+          this.form.controls.title.reset('');
+          this.form.controls.description.reset('');
+          this.queryRef.refetch();
+        },
+        (error) => console.error('Error: ', error)
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    GraphQLModule,
+    HttpClientModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
