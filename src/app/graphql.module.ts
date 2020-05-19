4f6e6335a453bede04366c21f17f664d8405114e
mezhik91 <mezhik91@gmail.com>
2020-05-19 09:25:08 +0200

    add token to request

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,12 +1,27 @@
+import { async } from '@angular/core/testing';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { NgModule } from '@angular/core';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+import { take } from 'rxjs/operators';
+import { ApolloLink } from 'apollo-link';
+
+const authCtx = (auth: AngularFireAuth) =>
+  setContext(async () => {
+    const token = await auth.idToken.pipe(take(1)).toPromise();
+    return {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+  });
 
 const uri = 'http://localhost:8081/v1/graphql'; // <-- add the URL of the GraphQL server here
-export function createApollo(httpLink: HttpLink) {
+export function createApollo(httpLink: HttpLink, auth: AngularFireAuth) {
   return {
-    link: httpLink.create({ uri }),
+    link: ApolloLink.from([authCtx(auth), httpLink.create({ uri })]),
     cache: new InMemoryCache(),
   };
 }
@@ -17,7 +32,7 @@ export function createApollo(httpLink: HttpLink) {
     {
       provide: APOLLO_OPTIONS,
       useFactory: createApollo,
-      deps: [HttpLink],
+      deps: [HttpLink, AngularFireAuth],
     },
   ],
 })
